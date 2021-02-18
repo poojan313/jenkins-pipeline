2f60da76a7245d9af50e6af95622eb71df108298
Poojan Khatri <Poojan.Khatri@iiitb.org>
2021-02-18 10:50:10 +0530

    Second HW

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     private String str="";
     public String getS(){
         System.out.println("Hello World");
+        System.out.println("Second Hello World");
         return str;
     }
 
